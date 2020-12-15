f192b589c8a87acc00700329c2a00257895236bb
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2020-12-15 17:56:40 -0300

    properly added google analytics 2

--- pages/_document.js
+++ pages/_document.js
@@ -11,19 +11,16 @@ class MyDocument extends Document {
       <Html lang="en">
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-185642244-1
-"
+          src="https://www.googletagmanager.com/gtag/js?id=UA-185642244-1"
         />
 
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-185642244-1
-');
-        `,
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-185642244-1');`,
           }}
         />
         <Head />
