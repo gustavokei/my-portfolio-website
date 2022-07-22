f3f7f2f75b1e9257e55b9077260fd1f5e6fd1ad9
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2022-07-22 08:34:23 -0300

    updating some information about myself

--- components/head.js
+++ components/head.js
@@ -2,7 +2,7 @@ import NextHead from "next/head";
 import { string } from "prop-types";
 
 const defaultDescription =
-  "I am a full stack web developer from Brazil. I worked as a web developer for around 3 years, while having a bachelor degree in Web Design (2018), and a post graduate degree in Web Design and Development (2021) ReactJS and NodeJS are currently my favourite libraries and my best project is gc-app-caps";
+  "I have been working as a web developer since 2016 while having a Brazilian Bachelor Degree in Web Design (2018) and a Canadian Graduate Certificate in Web Design and Development (2021) with high distinction.";
 const defaultOGURL = "";
 const defaultOGImage = "";
 
--- components/pages/home/intro.js
+++ components/pages/home/intro.js
@@ -7,10 +7,10 @@ const Intro = () => {
       <Col xs="10">
         <h1 className="title">My name is Gustavo Kei Amemiya</h1>
         <p>
-          I am a front-end developer from Brazil with more than 5 years of
-          experience in this position while having a Brazilian Bachelor Degree
-          in Web Design (2018) and a Canadian Graduate Certificate in Web Design
-          and Development (2021) with high distinction.
+          I have been working as a web developer since 2016 while having a
+          Brazilian Bachelor Degree in Web Design (2018) and a Canadian Graduate
+          Certificate in Web Design and Development (2021) with high
+          distinction.
         </p>
         <p>ReactJS and NodeJS are currently my favourite libraries!</p>
       </Col>
